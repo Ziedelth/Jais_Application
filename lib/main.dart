84555e20f761d7857c986d9e77a24523e973ca4d
Ziedelth <watteaupierre@gmail.com>
2022-02-14 10:41:53 +0100

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:jais/components/loading_widget.dart';
 import 'package:jais/utils/logger.dart';
 import 'package:jais/utils/main_color.dart';
 import 'package:jais/views/animes_view.dart';
