9f3de7806fe829abdac5ae72029ab54a5ebc2507
Ziedelth <watteaupierre@gmail.com>
2022-04-04 12:24:59 +0200

    Remove nn

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -159,6 +159,20 @@ class _SettingsViewState extends State<SettingsView> {
             ),
           ],
         ),
+        SectionWidget(
+          icon: const Icon(Icons.bug_report),
+          title: 'Mode développeur',
+          widgets: [
+            FullWidget(
+              widget: ElevatedButton(
+                child: const Text('Test'),
+                onPressed: () {
+
+                },
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
