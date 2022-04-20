a914028de63887229cf4b3ef03ddef94197f1948
Ziedelth <watteaupierre@gmail.com>
2022-04-20 20:01:12 +0200

    Fix

---
+++ lib/models/simulcast.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'simulcast.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Simulcast _$SimulcastFromJson(Map<String, dynamic> json) => Simulcast(
+      json['id'] as int,
+      json['simulcast'] as String,
+    );
+
+Map<String, dynamic> _$SimulcastToJson(Simulcast instance) => <String, dynamic>{
+      'id': instance.id,
+      'simulcast': instance.simulcast,
+    };
