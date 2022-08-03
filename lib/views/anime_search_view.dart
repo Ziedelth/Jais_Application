d244a37752e0b157d00700908b30282544f04b21
Ziedelth <watteaupierre@gmail.com>
2022-08-03 14:14:16 +0200

    Add historical search

--- lib/views/anime_search_view.dart
+++ lib/views/anime_search_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:jais/components/animes/anime_list.dart';
 import 'package:jais/components/animes/anime_widget.dart';
+import 'package:jais/components/jlist.dart';
 import 'package:jais/mappers/anime_mapper.dart';
+import 'package:jais/mappers/anime_search_notifier.dart';
 
 class AnimeSearchView extends StatefulWidget {
   const AnimeSearchView({super.key});
@@ -13,6 +15,45 @@ class AnimeSearchView extends StatefulWidget {
 class _AnimeSearchViewState extends State<AnimeSearchView> {
   final _animeMapper = AnimeMapper();
   final _animeWidgets = <Widget>[];
+  final _animeSearchNotifier = AnimeSearchNotifier();
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await _animeSearchNotifier.init();
+      setState(() {});
+    });
+  }
+
+  Future<void> _search(String query) async {
+    final animes = await _animeMapper.search(query: query);
+    if (animes == null) return;
+
+    _animeWidgets
+      ..clear()
+      ..addAll(
+        animes
+            .map<Widget>(
+              (e) => GestureDetector(
+            child: e,
+            onTap: () {
+              if (e is! AnimeWidget) return;
+              Navigator.pop(context);
+              Navigator.pushNamed(
+                context,
+                '/anime',
+                arguments: e.anime,
+              );
+            },
+          ),
+        )
+            .toList(),
+      );
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,36 +67,48 @@ class _AnimeSearchViewState extends State<AnimeSearchView> {
           ),
           autofocus: true,
           onSubmitted: (value) async {
-            final animes = await _animeMapper.search(query: value);
-            if (animes == null) return;
-
-            _animeWidgets
-              ..clear()
-              ..addAll(
-                animes
-                    .map<Widget>(
-                      (e) => GestureDetector(
-                        child: e,
-                        onTap: () {
-                          if (e is! AnimeWidget) return;
-                          Navigator.pop(context);
-                          Navigator.pushNamed(
-                            context,
-                            '/anime',
-                            arguments: e.anime,
-                          );
-                        },
-                      ),
-                    )
-                    .toList(),
-              );
-
-            setState(() {});
+            await _animeSearchNotifier.addSearch(value);
+            _search(value);
           },
         ),
       ),
       body: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
+          if (_animeSearchNotifier.hasSearch)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Historique de recherche',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ..._animeSearchNotifier
+                      .getSearch()
+                      .map<Widget>(
+                        (e) => ListTile(
+                          title: Text(e),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.clear),
+                            onPressed: () async {
+                              await _animeSearchNotifier.removeSearch(e);
+                              setState(() {});
+                            },
+                          ),
+                          onTap: () => _search(e),
+                        ),
+                      )
+                      .toList(),
+                ],
+              ),
+            ),
           Expanded(
             child: AnimeList(
               children: _animeWidgets,
